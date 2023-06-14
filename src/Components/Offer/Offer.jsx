542b777d4d0cfe447040f313e551b0c54bef2735
Prashant Srivastava <prashantsrivastava381@gmail.com>
2023-06-14 18:18:48 +0530

    Frontend UI Version-1

--- src/Components/Offer/Offer.jsx
+++ src/Components/Offer/Offer.jsx
@@ -12,7 +12,7 @@ const Offer = () => {
             <div className="offer-content-one">LIMITED OFFER</div>
             <div className="offer-content-two">35% off only this friday and get special gifts</div>
             <div className="offer-content-three">
-                 <div style={{paddingTop:"0.4rem",fontSize:"1.1rem"}}>Grab it now</div>
+                 <div className="grabButton">Grab it now</div>
                  <ArrowRightAltIcon fontSize='large'/>
             </div>
         </div>
