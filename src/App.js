a1ea511b7c173f9554921c70efb91196fb06dc9b
Prashant Srivastava <prashantsrivastava381@gmail.com>
2023-06-15 01:20:48 +0530

    Redux Add To Cart Made

--- src/App.js
+++ src/App.js
@@ -24,7 +24,7 @@ function App() {
           <Route path="/cart" element={<Cart/>} />
         </Routes>
         <Footer/>
-        <Toaster />
+        <Toaster position="bottom-left" toastOptions={{style:{zIndex:100000}}}/>
     </Router>
     </>
   );
