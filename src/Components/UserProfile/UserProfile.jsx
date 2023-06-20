ea6935f62599cd4b8c88f5f92c486f1d97ea8a3b
Prashant Srivastava <prashantsrivastava381@gmail.com>
2023-06-20 18:42:52 +0530

    User Profile Made Initial

--- src/Components/UserProfile/UserProfile.jsx
+++ src/Components/UserProfile/UserProfile.jsx
@@ -4,7 +4,7 @@ import './UserProfile.css'
 import { useDispatch, useSelector } from "react-redux";
 import { setLogout } from '../../Redux/Actions/product';
 import { getLocalData, removeLocalData } from '../../helpers/utils';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 const UserProfile = () => {
   const navigate = useNavigate();
@@ -17,14 +17,21 @@ const UserProfile = () => {
     navigate("/");
     removeLocalData("user")
   };
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, []);
   return (
     <>
       {userLoggedIn ? (
         <div className="user-profile">
-            <img src={user?.photoURL} alt="user" className="user-profile__img" />
-            <div className="user-profile__name">{user?.displayName}</div>
-            <div className="user-profile__email">{user?.email}</div>
-            <button className="login-button"  onClick={logoutHandler}>Logout</button>
+            <div className="user-top">
+              <img src={user?.photoURL} alt="user" className="user-profile__img" />
+              <div className="user-profile__name user-phone-view-details">{user?.displayName}</div>
+            </div>
+            <div className="user-bottom">
+              <div className="user-profile__email user-phone-view-details">{user?.email}</div>
+              <button className="login-button"  onClick={logoutHandler}>Logout</button>
+            </div>
         </div>
       ) : (
         <div className="user-profile">Login First</div>
